099e4db50c36ec9766ac826a6483cab8c75b1ead
sagar patil <sagarpatil98754@gmail.com>
2024-02-16 21:28:51 +0530

    add fine history

--- controllers/fineUser.js
+++ controllers/fineUser.js
@@ -1,20 +1,42 @@
 const Fine = require("../models/Fine")
 const User = require("../models/user")
 
-
 exports.FineUser = async (req,res)=>{
+    
     try{
-
         const {id} = req.params
+        const {fine_dl,fine_rc,fine_puc,fine_insurance,fine_total,fine_status,fined_user} = req.body; 
 
-        const data = req.body;
+        const savedFine = {
+            fine_dl : fine_dl,
+            fine_rc : fine_rc,
+            fine_puc : fine_puc,
+            fine_insurance : fine_insurance,
+            fine_total : fine_total,
+            fine_status : fine_status,
+            fined_user : id
+        }
 
-        console.log(data);  
-        https://github.com/SagarPatil007/DriveSafe-Backend.git
+        const user = await User.findById({_id:id});
 
+        if(fine_total != 0){
+            
+            const FineToUser = await Fine.create({
+                fine_dl, fine_rc, fine_puc, fine_insurance,
+                fine_status, fine_total, fined_user
+            });
+
+            await User.updateOne({_id:user._id},{
+                $push : {
+                    'fine_history': FineToUser._id,
+                }},
+                { multi: true },
+            )
+        }
+        
         return res.status(200).json({
             "success": true,
-            // "Data": savedFine,
+            "Data": savedFine,
             "message": "Fine Allocation done",
         })
 
